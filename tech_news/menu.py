c6e7dff1deaeb098b4694d201dcb49db3d12e791
GustavoDimeira <gustavodimeira1@gmail.com>
2023-05-07 18:54:45 -0400

    feat: show options to analyzer_menu function

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,18 @@
+import sys
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    try:
+        option = int(input("""Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por categoria;
+ 4 - Listar top 5 categorias;
+ 5 - Sair.\n"""))
+        if (option < 0 or option > 5):
+            raise ValueError
+
+    except ValueError:
+        sys.stderr.write('Invalid option\n')
